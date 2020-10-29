70649b994483f0537d898c64c42c90fc260e5330
DPDivesh <diveshpatel39@gmail.com>
2020-10-28 20:17:15 -0700

    added functions for computers random choice

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 
+//function to initialize variables the computer may choose
 function computerPlay(){
 let rock = 1;
 let paper = 2;
 let scissors = 3;
-
-
+let computerInput = getRandomInRange(1,3);
+console.log(computerInput);
 
 }
+
+
+
+
+//function called to give random integer for computers variable choice 
+function getRandomInRange(min, max) {
+  //gets a integer value below the max value and at or above the minimum
+  //avoid the 0 being issued for practice by adding min in case of 0 inclusion 
+  let result = Math.floor(Math.random() * (max) + min);
+  console.log(result);
+  return result;
+}
+computerPlay();
